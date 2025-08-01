1b8525af07ce6dd680194289450fcc2b680b2813
arup010 <arup13556@gmail.com>
2025-08-02 03:21:12 +0530

    feat: integrate @tanstack/router for routing and enhance homepage with animated particles
    - Updated ESLint configuration for consistency in import statements.
    - Added @tanstack/router-plugin to package.json for routing capabilities.
    - Modified main.tsx to implement routing with RouterProvider and create routes.
    - Created Homepage component with animated particles effect.
    - Developed Homelayout component for consistent layout across pages.
    - Updated Vite configuration to include tanstackRouter plugin.
    - Adjusted index.css to ensure proper styling.

---
+++ src/layout/Home/Home_layout.tsx
@@ -0,0 +1,103 @@
+import react, { useState } from "react";
+import { Link, Outlet } from "@tanstack/react-router";
+import { motion } from "motion/react";
+export function Homelayout(): react.JSX.Element {
+  
+
+  const [selectedTab, setSelectedTab] = useState(1);
+  const handleTabClick = (tabIndex: number) => {
+    setSelectedTab(tabIndex);
+  };
+  return (
+    <div className="">
+      <div className="flex justify-around fixed top-0 left-0 right-0 z-50 flexjustify-between py-6 px-8 bg-gradient-to-r from-purple-900/80 via-indigo-900/80 to-blue-900/80 backdrop-blur-md border-b border-white/10">
+        <div className="flex justify-start ">
+          <span className="">HIT AEIE</span>
+        </div>
+        <div className="flex  gap-4 text-gray-400">
+          <Link
+            to="/"
+            className="font-mono  [&.active]:text-teal-500  "
+            onClick={() => handleTabClick(1)}
+          >
+            {""}Home
+            {selectedTab === 1 ? (
+             <motion.div
+                    className="absolute   h-0.5 w-9 bg-gray-900 rounded-full"
+                    layoutId="underline"
+                    initial={{ opacity: 0, scaleX: 0 }}
+                    animate={{ opacity: 1, scaleX: 1 }}
+                    transition={{ duration: 0.3, ease: "easeInOut" }}
+                  />
+            ) : null}
+          </Link>
+          <Link
+            to="/Events"
+            className="font-mono [&.active]:text-teal-500"
+            onClick={() => handleTabClick(2)}
+          >
+            {""}Events
+            {selectedTab === 2 ? (
+              <motion.div
+                    className="absolute   h-0.5 w-13 bg-gray-900 rounded-full"
+                    layoutId="underline"
+                    initial={{ opacity: 0, scaleX: 0 }}
+                    animate={{ opacity: 1, scaleX: 1 }}
+                    transition={{ duration: 0.3, ease: "easeInOut" }}
+                  />
+            ) : null}
+          </Link>
+          <Link
+            to="/Study"
+            className="font-mono [&.active]:text-teal-500"
+            onClick={() => handleTabClick(3)}
+          >
+            {""}Study
+            {selectedTab === 3 ? (
+             <motion.div
+                    className="absolute   h-0.5 w-9 bg-gray-900 rounded-full"
+                    layoutId="underline"
+                    initial={{ opacity: 0, scaleX: 0 }}
+                    animate={{ opacity: 1, scaleX: 1 }}
+                    transition={{ duration: 0.3, ease: "easeInOut" }}
+                  />
+            ) : null}
+          </Link>
+          <Link
+            to="/About"
+            className="font-mono [&.active]:text-teal-500"
+            onClick={() => handleTabClick(4)}
+          >
+            {""}About
+            {selectedTab === 4 ? (
+              <motion.div
+                    className="absolute   h-0.5 w-10 bg-gray-900 rounded-full"
+                    layoutId="underline"
+                    initial={{ opacity: 0, scaleX: 0 }}
+                    animate={{ opacity: 1, scaleX: 1 }}
+                    transition={{ duration: 0.3, ease: "easeInOut" }}
+                  />
+            ) : null}
+          </Link>
+          <Link
+            to="/contectUs"
+            className="font-mono [&.active]:text-teal-500"
+            onClick={() => handleTabClick(5)}
+          >
+            {""}Contect us
+            {selectedTab === 5 ? (
+              <motion.div
+                    className="absolute   h-0.5 w-23 bg-gray-900 rounded-full"
+                    layoutId="underline"
+                    initial={{ opacity: 0, scaleX: 0 }}
+                    animate={{ opacity: 1, scaleX: 1 }}
+                    transition={{ duration: 0.3, ease: "easeInOut" }}
+                  />
+            ) : null}
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  RouterProvider,
+  createRootRoute,
+  createRoute,
+  createRouter,
+} from "@tanstack/react-router";
+import { Homelayout } from "./layout/Home/Home_layout";
+import { Homepage } from "./pages/home/Home";
 
+const rootrouter = createRootRoute({
+  component: Homelayout,
+});
+const Homeroute = createRoute({
+  getParentRoute: () => rootrouter,
+  path: "/",
+  component: Homepage,
+});
 
-createRoot(document.getElementById('root')!).render(
+const routeTree = rootrouter.addChildren([Homeroute]);
+const router = createRouter({ routeTree });
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
