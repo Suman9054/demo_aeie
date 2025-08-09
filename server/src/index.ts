8689138885569887a418b826fc43988ce39a3ef2
Suman9054 <sumanhazra368@gmail.com>
2025-08-10 00:03:18 +0530

    Refactor database connection and user management functions; add event handling and registration logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
 import { Hono } from "hono";
-import { db_conect } from "./db/db";
+import { db_connect } from "./db/db";
 
 const app = new Hono();
 
-db_conect(); 
+db_connect();
 export default {
   port: 3000,
   fetch: app.fetch,
